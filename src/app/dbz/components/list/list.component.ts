933e39aa59cbd564253d6cec4c397f6e15c12310
HUGO MENESES <hugolterrible@gmail.com>
2024-07-19 20:39:03 -0600

    Readme update 19072024

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/chatacter.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [//Por default o vacio
+    {name:'Trunk',power:10}
+  ];
+
+  @Output()
+  //public onDelete= new EventEmitter<number>();
+  public onDelete:EventEmitter<number> = new EventEmitter();
+
+
+  onDeleteCharacter(index:number):void{
+    //Emitir el ID del personaje
+    this.onDelete.emit(index)
+    debugger;
+    console.log({index});
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/chatacter.interface';
+import { AddCharacterComponent } from '../components/add-character/add-character.component';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
-  constructor() { }
+export class MainPageComponent {
 
-  ngOnInit() { }
+public characters: Character[] = [
+  {name:'Krillins',power:1000},
+  {name:'Goku',power:9500},
+  {name:'Vegeta',power:7500}
+];
+
+onNewCharacter(character:Character):void{
+  //this.characters.unshift(character); //inserta al princio
+  this.characters.push(character);
+console.log('MainPage');
+console.log(character);
+}
+
+onDeleteCharacter(index:number){
+  this.characters.splice(index,1);
+}
+
 }
